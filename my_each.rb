d8039d9c37e4400fa18958fb9dc812701bc8e0c2
Luis Zabala <luis.zabala076@gmail.com>
2019-01-15 18:04:09 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,8 @@
-require 'pry'
-
 def my_each(array)
-  i=0
+  i = 0 
   while i < array.length
   yield array[i]
-    i = i+1
-  require 'pry'
-end
-return array  
+  i +=1
+end 
+return array
 end
